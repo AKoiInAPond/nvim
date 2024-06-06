ff4cb294e35c35630b2312fe61d3b11500ebcd64
mai <miguelandradeb0@gmail.com>
2024-06-06 21:06:44 +0800

    added set.lua

--- lua/koi/init.lua
+++ lua/koi/init.lua
@@ -1,1 +1,2 @@
 require("koi.remap")
+require("koi.set")
---
+++ lua/koi/set.lua
@@ -0,0 +1,14 @@
+vim.opt.nu =true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
